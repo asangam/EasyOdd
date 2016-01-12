61fef2e478d4cf591b987cf23ae1d1840d373198
asangam <san.adh2010@gmail.com>
2016-01-12 18:43:34 +0545

    C# Program
    A program demonstrating the odd number from 1 to 500

---
+++ EasyOdd/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyOdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            for (i = 1; i <= 500; i+=2)
+                Console.Write("" + i + "\n");
+            Console.ReadLine();
+
+        }
+    }
+}
